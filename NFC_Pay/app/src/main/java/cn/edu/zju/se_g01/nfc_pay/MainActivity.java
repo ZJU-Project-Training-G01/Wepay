a6fe3eacf3ecbda1738f931f9842cb705f9d2856
singleghost <570111267@qq.com>
2017-07-06 10:19:38 +0800

    创建 Tabbar

--- NFC_Pay/app/src/main/java/cn/edu/zju/se_g01/nfc_pay/MainActivity.java
+++ NFC_Pay/app/src/main/java/cn/edu/zju/se_g01/nfc_pay/MainActivity.java
@@ -1,45 +1,90 @@
 package cn.edu.zju.se_g01.nfc_pay;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.zju.se_g01.nfc_pay.fragments.HomeFragment;
+import cn.edu.zju.se_g01.nfc_pay.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int[] icons = {R.drawable.tab_home,
+                R.drawable.tab_home,
+                R.drawable.tab_home,
+                R.drawable.tab_home,
+        };
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_tab_content);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setupViewPager(viewPager);
+
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < icons.length; i++) {
+            tabLayout.getTabAt(i).setIcon(icons[i]);
+        }
+        tabLayout.getTabAt(0).select();
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.insertNewFragment(new HomeFragment());
+        adapter.insertNewFragment(new SearchFragment());
+        adapter.insertNewFragment(new HomeFragment());
+        adapter.insertNewFragment(new SearchFragment());
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void insertNewFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = this.getMenuInflater();
+//        inflater.inflate(R.menu.main_activity, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//
+//            case R.id.action_settings:
+//                Intent settingsIntent = new Intent(this, TabSample.class);
+//                startActivity(settingsIntent);
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
---
+++ NFC_Pay/app/src/main/java/cn/edu/zju/se_g01/nfc_pay/fragments/SearchFragment.java
@@ -0,0 +1,22 @@
+package cn.edu.zju.se_g01.nfc_pay.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.edu.zju.se_g01.nfc_pay.R;
+
+/**
+ * Created by dddong on 2017/7/5.
+ */
+
+public class SearchFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.search_layout, container, false);
+    }
+}
