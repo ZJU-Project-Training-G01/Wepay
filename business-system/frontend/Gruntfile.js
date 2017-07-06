ed341cacd953e3d6fa14b6cda49f1cde4cae2cbe
achao <zhangshichao628@sina.com>
2017-07-06 11:46:31 +0800

    grunt init

---
+++ business-system/frontend/Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            output: {
+                files: [{
+                    expand: true,
+                    cwd: 'frontend/src',
+                    src: '**/main.scss',
+                    dest: 'frontend/build',
+                    ext: '.css'
+                }]
+
+            }
+        },
+        concat: {
+            options: {
+                //
+            },
+            dist: {
+                src: ['frontend/src/js/*'],
+                dest: 'frontend/build/js/global.js'
+            }
+        },
+        uglify: {
+            compressjs: {
+                files: {
+                    'frontend/build/js/global.min.js': ['frontend/build/js/global.js']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                // stripBanners:true,
+            },
+            build: {
+                src: 'frontend/build/css/main.css',
+                dest: 'frontend/build/css/main.min.css'
+            }
+        },
+        jshint: {
+            all: ['frontend/build/js/global.js']
+        },
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
+                collapseBooleanAttributes: true,
+                useShortDoctype: true,
+                removeAttributeQuotes: true,
+                removeRedundantAttributes: true,
+                removeEmptyAttributes: true,
+                removeOptionalTags: true
+            },
+            html: {
+                files: [{
+                    expand: true,
+                    cwd: 'frontend/src/html',
+                    src: ['*.html'],
+                    dest: 'frontend/build/html'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['frontend/src/html/*.html', 'frontend/src/js/*.js', 'frontend/src/css/main.scss'],
+                tasks: ['sass', 'htmlmin', 'cssmin', 'concat', 'jshint', 'uglify']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%=connect.options.livereload %>'
+                },
+                files: [
+                    'frontend/build/js/global.min.js',
+                    'frontend/build/css/main.min.css',
+                    'frontend/build/html/*'
+
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9001,
+                open: true,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            server: {
+                options: {
+                    port: 9001,
+                    base: "./"
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('outputcss', ['sass']);
+    grunt.registerTask('default');
+};
