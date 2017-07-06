ae7210f8e6740eaeed553b66dfe67982b6017196
achao <zhangshichao628@sina.com>
2017-07-06 14:52:24 +0800

    grunt sass 配置以及设置订单和商品的图标

--- business-system/frontend/Gruntfile.js
+++ business-system/frontend/Gruntfile.js
@@ -1,79 +1,29 @@
 module.exports = function(grunt) {
+    var sidebarPath = 'components/sidebar';
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
             output: {
                 files: [{
                     expand: true,
-                    cwd: 'frontend/src',
-                    src: '**/main.scss',
-                    dest: 'frontend/build',
+                    cwd: sidebarPath,
+                    src: 'sidebar.scss',
+                    dest: sidebarPath,
                     ext: '.css'
-                }]
-
-            }
-        },
-        concat: {
-            options: {
-                //
-            },
-            dist: {
-                src: ['frontend/src/js/*'],
-                dest: 'frontend/build/js/global.js'
-            }
-        },
-        uglify: {
-            compressjs: {
-                files: {
-                    'frontend/build/js/global.min.js': ['frontend/build/js/global.js']
-                }
-            }
-        },
-        cssmin: {
-            options: {
-                // stripBanners:true,
-            },
-            build: {
-                src: 'frontend/build/css/main.css',
-                dest: 'frontend/build/css/main.min.css'
-            }
-        },
-        jshint: {
-            all: ['frontend/build/js/global.js']
-        },
-        htmlmin: {
-            options: {
-                removeComments: true,
-                collapseWhitespace: true,
-                collapseBooleanAttributes: true,
-                useShortDoctype: true,
-                removeAttributeQuotes: true,
-                removeRedundantAttributes: true,
-                removeEmptyAttributes: true,
-                removeOptionalTags: true
-            },
-            html: {
-                files: [{
-                    expand: true,
-                    cwd: 'frontend/src/html',
-                    src: ['*.html'],
-                    dest: 'frontend/build/html'
                 }]
             }
         },
         watch: {
             scripts: {
-                files: ['frontend/src/html/*.html', 'frontend/src/js/*.js', 'frontend/src/css/main.scss'],
-                tasks: ['sass', 'htmlmin', 'cssmin', 'concat', 'jshint', 'uglify']
+                files: [sidebarPath + '/sidebar.scss'],
+                tasks: ['sass']
             },
             livereload: {
                 options: {
                     livereload: '<%=connect.options.livereload %>'
                 },
                 files: [
-                    'frontend/build/js/global.min.js',
-                    'frontend/build/css/main.min.css',
-                    'frontend/build/html/*'
+                    sidebarPath + '/siderbar.css'
 
                 ]
             }
@@ -92,8 +42,15 @@ module.exports = function(grunt) {
                 }
             }
         }
+
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect')
+
+
     grunt.registerTask('outputcss', ['sass']);
+    grunt.registerTask('watchit', ['outputcss', 'connect', 'watch']);
+
     grunt.registerTask('default');
 };
