e7a8a11cc7152af2f3d31fb0b5b15c164003fb0e
singleghost <570111267@qq.com>
2017-07-09 14:18:10 +0800

    修改接口文档

--- NFC_Pay/app/src/main/java/cn/edu/zju/se_g01/nfc_pay/EditUserInfoActivity.java
+++ NFC_Pay/app/src/main/java/cn/edu/zju/se_g01/nfc_pay/EditUserInfoActivity.java
@@ -2,6 +2,8 @@ package cn.edu.zju.se_g01.nfc_pay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,10 +15,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import cn.edu.zju.se_g01.nfc_pay.tools.CookieRequest;
+import cn.edu.zju.se_g01.nfc_pay.tools.MySingleton;
 
 public class EditUserInfoActivity extends AppCompatActivity {
 
     private final static String sUrl = "http://localhost/getUserInfo.php";
+    private static final String TAG = "EditUserInfoActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,7 @@ public class EditUserInfoActivity extends AppCompatActivity {
         final EditText addressEditText = (EditText)findViewById(R.id.addressEditText);
         Button userInfoCommitBtn = (Button)findViewById(R.id.userInfoCommitBtn);
 
+        //发起一个网络请求，获取用户信息
         CookieRequest userInfoReq = new CookieRequest(getApplicationContext(), Request.Method.GET, sUrl,
                 null, new Response.Listener<JSONObject>() {
             @Override
@@ -34,7 +40,8 @@ public class EditUserInfoActivity extends AppCompatActivity {
                 try {
                     JSONObject data = response.getJSONObject("data");
                     realNameEditText.setText(data.getString("realName"));
-                    phoneEditText.setText(data.getString(""));
+                    phoneEditText.setText(data.getString("phone"));
+                    addressEditText.setText(data.getString("address"));
                 } catch (JSONException e) {
                     e.printStackTrace();
 
@@ -43,6 +50,14 @@ public class EditUserInfoActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
+        MySingleton.getInstance(getApplicationContext()).getRequestQueue().add(userInfoReq);
+
+        userInfoCommitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
