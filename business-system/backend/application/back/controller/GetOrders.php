d26110c1f91467fb2a32ece9a0126c8fc38405d3
mark_who <1296416170@qq.com>
2017-07-10 14:58:38 +0800

    修改SellersGetOrders.php”
    exit

--- business-system/backend/application/back/controller/GetOrders.php
+++ business-system/backend/application/back/controller/GetOrders.php
@@ -16,20 +16,30 @@ class GetOrders extends Controller
     public function GetOrders()
     {
         $request = Request::instance();
-        //$pageNumber = 1;
+        //$pageNumber = 2;
         //$pageSize = 2;
+        //$status = 0;
         $pageSize = $request->post('pageSize');
         $pageNumber = $request->post('pageNumber');
+        $status = $request->post('status');
         $recordP = ($pageNumber-1)*$pageSize;
-        try {
-            $data = Db::query('select * from orders limit :recordP,:pageSize', ['recordP' => $recordP, 'pageSize' => $pageSize]);
+        if($status == -1)
+        {
+            try {
+                $data = Db::query('select * from orders limit :recordP,:pageSize', ['recordP' => $recordP, 'pageSize' => $pageSize]);
+                $code = 0;
+                $msg = '';
+            } catch (Exception $e) {
+                $code = 2;
+                $msg = $e->getMessage();
+                $data = NULL;
+            }
+        }
+        else
+        {
+            $data = Db::query('select * from orders where orderStatus = :status limit :recordP,:pageSize', ['recordP' => $recordP, 'pageSize' => $pageSize, 'status'=>$status]);
             $code = 0;
             $msg = NULL;
-        }
-        catch (Exception $e){
-            $code = 2;
-            $msg = $e->getMessage();
-            $data = NULL;
         }
         $res = [
             'code' => $code,
