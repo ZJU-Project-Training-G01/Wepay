159bc4af0bce48ab9fac906e13835ae2d7656300
achao <zhangshichao628@sina.com>
2017-07-10 13:49:57 +0800

    测试接口

--- business-system/frontend/pages/goodDetail/goodDetail.js
+++ business-system/frontend/pages/goodDetail/goodDetail.js
@@ -1,5 +1,11 @@
 (function() {
     angular.module('goodDetail', [])
-        .controller('goodDetail', ['$scope', function($scope) {}])
+        .controller('goodDetail', ['$scope', function($scope) {
+            console.log('zz');
+        }])
+        .component('goodItemDetail', {
+            templateUrl: 'frontend/components/goodItemDetail/goodItemDetail.html',
+            controller: 'goodItemDetailController'
+        });
 
 })();
