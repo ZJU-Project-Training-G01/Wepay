3e1a69d25f5c2f6cad8195e88245b44e8adad213
achao <zhangshichao628@sina.com>
2017-07-11 09:46:46 +0800

    上传王凯要用的两个库文件“

--- business-system/frontend/components/goodSearch/goodSearch.js
+++ business-system/frontend/components/goodSearch/goodSearch.js
@@ -3,7 +3,10 @@
          .module('goodSearch', [])
          .controller('goodSearchCtrl', ['$scope', '$http',
              function($scope, $http) {
-
+                 $scope.search = function() {
+                     let keyword =
+                         $scope.$emit('transferKeyword')
+                 }
              }
          ])
  })();
