3dbfb9832a508fe1b486cd3991354306e533e1ff
achao <zhangshichao628@sina.com>
2017-07-12 21:33:03 +0800

    商品组件增加成交量

--- business-system/frontend/components/goodItem/goodItem.js
+++ business-system/frontend/components/goodItem/goodItem.js
@@ -7,6 +7,9 @@
                 $scope.toDetail = function(good) {
                     $location.path('good/' + good.goodId);
                 };
+                $scope.$on('receiveKeyword', function(e, keyword) {
+                    $scope.goodHttp($scope.pageNumber, keyword);
+                });
                 $scope.goodHttp = function(pageNumber, keyword) {
                     $http({
                         url: 'frontend/static/jsons/goods.json',
