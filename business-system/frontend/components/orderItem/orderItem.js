2aef14f9d0988a881748d6ebf9c92193c0f6f0cd
achao <zhangshichao628@sina.com>
2017-07-12 10:52:53 +0800

    完成发货的业务逻辑

--- business-system/frontend/components/orderItem/orderItem.js
+++ business-system/frontend/components/orderItem/orderItem.js
@@ -9,6 +9,20 @@
                     $scope.status = status;
                     $scope.orderHttp($scope.pageNumber);
                 });
+                $scope.sendGood = function(item) {
+                    $http({
+                        url: 'frontend/static/jsons/sendGood.json',
+                        method: 'post',
+                        data: { orderId: item.orderId }
+                    }).then(function(data) {
+                        let code = data.data.code;
+                        if (code === 0) {
+                            item.orderStatus = 1;
+                            item.orderName = '买家未收货';
+                            item.operation = '等待买家收货';
+                        }
+                    })
+                }
                 $scope.orderHttp = function(pageNumber) {
                     $http({
                         method: 'post',
