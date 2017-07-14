3f5da56ef2c0e6d1fcd8ae1f7ba4b95512788432
achao <zhangshichao628@sina.com>
2017-07-14 14:59:48 +0800

    修复未传手机号的bug

--- business-system/frontend/components/bind/bind.js
+++ business-system/frontend/components/bind/bind.js
@@ -1,7 +1,7 @@
 (function() {
     angular.module('bind', [])
-        .controller('bind', ['$scope', '$location', '$window',
-            function($scope, $location, $window) {
+        .controller('bind', ['$scope', '$location', '$window', '$http',
+            function($scope, $location, $window, $http) {
                 $scope.bind = function() {
                     if ($scope.bankCard === undefined) {
                         $scope.$emit('transferErrorMsg', '银行卡号不得为空');
@@ -19,6 +19,17 @@
                         $scope.$emit('transferErrorMsg', '密码不得为空');
                         return;
                     }
+                    $http({
+                        url: 'frontend/static/jsons/bind.json',
+                        method: 'post',
+                        data: { bankCard: $scope.bankCard }
+                    }).then(function(data) {
+                        let code = data.data.code;
+                        if (code === 0) {
+                            $window.location.reload();
+                            $scope.$emit('transferErrorMsg', '绑定成功', true);
+                        }
+                    })
                 };
                 $scope.cancel = function() {
                     $window.location.reload();
