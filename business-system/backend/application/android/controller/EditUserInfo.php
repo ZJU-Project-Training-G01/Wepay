b2038a0e2213855d5d0b3848bea841bf9ee0e48d
mark_who <1296416170@qq.com>
2017-07-14 10:32:24 +0800

    修改部分不符合规范的数据库操作语句

--- business-system/backend/application/android/controller/EditUserInfo.php
+++ business-system/backend/application/android/controller/EditUserInfo.php
@@ -23,25 +23,28 @@ class EditUserInfo extends Controller
         $realName = $request->post('realName');
         $phoneNumber = $request->post('phone');
         $address = $request->post('address');
+
+        //test
         /*$buyerName = '小敏';
         $realName = 'xiaoming';
         $phoneNumber = 10016;
-        $address = 'baita';*/
+        $address = 'baita';
+        Session::set('login', 'true');*/
 
 
         if($request->session('login')!='true')
         {
             //返回上个页面；
-            $code = 1;
+            $code = 2;
             $msg = '用户未登录';
             $data = NULL;
         }
         else
         {
             $buyerId = $request->session('buyer_id');
-            //$buyerId = 1;
+            //$buyerId = 3;
             try {
-                $data = Db::query('update buyer set buyerName = :buyerName, realName = :realName, phoneNumber = :phoneNumber, address = :address where buyerId = :buyerId',
+                $data = Db::execute('update buyer set buyerName = :buyerName, realName = :realName, phoneNumber = :phoneNumber, address = :address where buyerId = :buyerId',
                     ['buyerName' => $buyerName, 'realName' => $realName, 'phoneNumber' => $phoneNumber, 'address' => $address, 'buyerId' => $buyerId]);
                 $code = 0;
                 $msg = NULL;
